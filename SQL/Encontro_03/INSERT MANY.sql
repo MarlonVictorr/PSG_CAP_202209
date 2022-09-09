d19531e5e7262998fc28473af438273950f07b19
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-09-09 01:07:10 -0300

    Create INSERT MANY.sql
    Inserção de dados para cada Cliente contendo as seguintes informações nome, documento, telefone, email, endereco

---
+++ SQL/Encontro_03/INSERT MANY.sql
@@ -0,0 +1,11 @@
+INSERT INTO Cliente(Nome,Documento,Telefone,Email,Endereco)
+VALUES
+('Lucas','456','987654','lucas@teste.com','Rua x,n.11'),
+('Joao','789','987655','joao@teste.com','Rua x,n.12'),
+('Pedro','657','987656','pedro@teste.com','Rua x,n.13'),
+('Tiago','324','987657','tiago@teste.com','Rua x,n.14'),
+('Judas','052','987658','judas@teste.com','Rua x,n.15'),
+('Marcos','632','987659','marcos@teste.com','Rua x,n.16'),
+('André','890','987660','andré@teste.com','Rua x,n.17')
+GO
+
