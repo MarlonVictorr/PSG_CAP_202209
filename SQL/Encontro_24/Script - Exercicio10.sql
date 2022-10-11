77aae818fdad2130b63c2f90a499bac75e18cf4f
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-10-10 20:47:12 -0400

    Create Script - Exercicio10.sql
    Prova sql

---
+++ SQL/Encontro_24/Script - Exercicio10.sql
@@ -0,0 +1,47 @@
+
+--DECLARE @Id_Tipo_Aquicultura INT
+--SET @Id_Tipo_Aquicultura = 20
+
+--DECLARE @Id_Municipio INT
+--SET @Id_Municipio = 1100015
+
+--DECLARE @Ano INT
+--SET @Ano = 2013
+
+--SELECT Aquicultura.IdAquicultura, 
+--	   Aquicultura.Ano, 
+--	   Aquicultura.IdMunicipio, 
+--	   tipo.IdTipoAquicultura,
+--	   Tipo.DescricaoTipoAquicultura, 
+--	   Aquicultura.Producao, 
+--	   Aquicultura.ValorProducao, 
+--	   Aquicultura.ProporcaoValorProducao,
+--	   Aquicultura.Moeda
+
+--FROM Aquicultura 
+--INNER JOIN TipoAquicultura AS Tipo ON Aquicultura.IdTipoAquicultura = Tipo.IdTipoAquicultura
+--WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND Id_Municipio = @Id_Municipio AND Aquicultura.Producao IS NOT NULL AND Aquicultura.ValorProducao IS NOT NULL
+--GO
+
+CREATE PROCEDURE SP_EX10
+@Id_Tipo_Aquicultura INT, @Id_Municipio INT, @Ano INT
+AS
+BEGIN
+	SELECT Aquicultura.IdAquicultura, 
+	   Aquicultura.Ano, 
+	   Aquicultura.IdMunicipio, 
+	   tipo.IdTipoAquicultura,
+	   Tipo.DescricaoTipoAquicultura, 
+	   Aquicultura.Producao, 
+	   Aquicultura.ValorProducao, 
+	   Aquicultura.ProporcaoValorProducao,
+	   Aquicultura.Moeda
+
+	FROM Aquicultura 
+	INNER JOIN TipoAquicultura AS Tipo ON Aquicultura.IdTipoAquicultura = Tipo.IdTipoAquicultura
+	WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND Id_Municipio = @Id_Municipio AND Aquicultura.Producao IS NOT NULL AND Aquicultura.ValorProducao IS NOT NULL
+END
+GO
+
+EXEC SP_EX10 20, 1100015, 2013
+GO
