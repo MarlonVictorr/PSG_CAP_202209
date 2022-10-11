b532210f3a7fa2f7d304ef162d68eddd6a2f751e
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-10-11 00:00:37 -0400

    Update Script - Exercicio07.sql

--- SQL/Encontro_24/Script - Exercicio07.sql
+++ SQL/Encontro_24/Script - Exercicio07.sql
@@ -8,19 +8,19 @@
 --DECLARE @Ano INT
 --SET @Ano = 2013
 
---SELECT Aquicultura.Id_Aquicultura, 
+--SELECT Aquicultura.IdAquicultura, 
 --	   Aquicultura.Ano, 
---	   Aquicultura.Id_Municipio, 
---	   tipo.Id_Tipo_Aquicultura,
---	   Tipo.Descricao_Tipo_Aquicultura, 
+--	   Aquicultura.IdMunicipio, 
+--	   tipo.IdTipoAquicultura,
+--	   Tipo.DescricaoTipoAquicultura, 
 --	   Aquicultura.Producao, 
---	   Aquicultura.Valor_Producao, 
---	   Aquicultura.Proporcao_Valor_Producao,
+--	   Aquicultura.ValorProducao, 
+--	   Aquicultura.ProporcaoValorProducao,
 --	   Aquicultura.Moeda
 
 --FROM Aquicultura 
 --INNER JOIN TipoAquicultura AS Tipo ON Aquicultura.IdTipoAquicultura = Tipo.IdTipoAquicultura
---WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND Id_Municipio = @Id_Municipio AND Ano = @Ano
+--WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND IdMunicipio = @Id_Municipio AND Ano = @Ano
 --GO
 
  
@@ -38,7 +38,7 @@ BEGIN
 	   Aquicultura.Moeda
 	FROM Aquicultura 
 	INNER JOIN TipoAquicultura AS Tipo ON Aquicultura.IdTipoAquicultura = Tipo.IdTipoAquicultura
-	WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND Id_Municipio = @Id_Municipio AND Ano = @Ano
+	WHERE tipo.IdTipoAquicultura = @Id_Tipo_Aquicultura AND IdMunicipio = @Id_Municipio AND Ano = @Ano
 END
 GO
 
