4522d6b0ff57f328296f06fff1e7937376eff407
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-10-10 20:32:04 -0400

    Create Script - Exercicio08.sql
    Prova sql

---
+++ SQL/Encontro_24/Script - Exercicio08.sql
@@ -0,0 +1,25 @@
+
+--DECLARE @Id_Municipio INT
+--SET @Id_Municipio = 1100015
+
+--DECLARE @Ano INT
+--SET @Ano = 2013
+
+--SELECT * FROM VIEW VW_Aquicultura
+--WHERE Id_Municipio = @Id_Municipio AND Ano = @Ano AND Producao IS NOT NULL
+--GO
+
+CREATE PROCEDURE SP_EX08
+@Id_Municipio INT, @Ano INT
+AS
+BEGIN
+	SELECT * FROM VW_Aquicultura
+	WHERE Id_Municipio = @Id_Municipio AND Ano = @Ano AND Producao IS NOT NULL
+END
+GO
+
+EXEC SP_EX08 1100015, 2013
+GO
+
+
+select * from Raw_Aquicultura_Brasil_Municipios
