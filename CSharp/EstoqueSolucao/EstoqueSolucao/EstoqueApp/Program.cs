6e037260969cb0faceb391c663602b0eada8da0e
MarlonVictorr <112974081+MarlonVictorr@users.noreply.github.com>
2022-10-24 17:47:42 -0400

    Encontro 32
    Trabalhamos com Crud implementando um repositorio,junto com um FakeDB

--- CSharp/EstoqueSolucao/EstoqueSolucao/EstoqueApp/Program.cs
+++ CSharp/EstoqueSolucao/EstoqueSolucao/EstoqueApp/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 
-using EstoqueApp.Model.Estoque;
-
 namespace EstoqueApp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Categoria cat = new Categoria(1, "Teste", true, DateTime.Now);
-            cat.Imprimir();
+
         }
     }
 }
